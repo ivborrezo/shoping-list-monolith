acaedf62c96034862f866d16224b6f33927f431d
= <=>
2024-03-20 20:41:53 +0100

    Initial commit

---
+++ src/main/java/es/ivborrezo/shopinglistmonolith/ShopingListMonolithApplication.java
@@ -0,0 +1,13 @@
+package es.ivborrezo.shopinglistmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingListMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingListMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ivborrezo/shopinglistmonolith/ShopingListMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ivborrezo.shopinglistmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingListMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
