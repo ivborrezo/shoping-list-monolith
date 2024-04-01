df08a8563d49d457a783cb3750dde76e518f090c
= <=>
2024-04-01 13:42:26 +0200

    Add contextLoad test to avoid showing disabled

--- src/test/java/es/ivborrezo/shoppinglistmonolith/ShoppingListMonolithApplicationTests.java
+++ src/test/java/es/ivborrezo/shoppinglistmonolith/ShoppingListMonolithApplicationTests.java
@@ -1,15 +1,25 @@
 package es.ivborrezo.shoppinglistmonolith;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import es.ivborrezo.shoppinglistmonolith.user.UserController;
 
 @SpringBootTest
 class ShoppingListMonolithApplicationTests {
 
+	@Autowired
+	UserController controller;
+	
 	@Test
-	@Disabled
-	void contextLoads() {
+	void contextLoads() throws Exception {
+	
+		assertThat(controller).isNotNull();
+	
 	}
 
 }
