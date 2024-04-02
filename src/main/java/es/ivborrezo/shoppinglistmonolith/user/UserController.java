e3ae7aa1480c0ca137b1da3bcb7c55ed717add00
= <=>
2024-04-02 22:09:29 +0200

    Add /users endpoint with pagination

--- src/main/java/es/ivborrezo/shoppinglistmonolith/user/UserController.java
+++ src/main/java/es/ivborrezo/shoppinglistmonolith/user/UserController.java
@@ -1,10 +1,12 @@
 package es.ivborrezo.shoppinglistmonolith.user;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +20,17 @@ public class UserController {
 	public UserController(UserService userService, UserOutputDTOMapper userOutputDTOMapper) {
 		this.userService = userService;
 		this.userOutputDTOMapper = userOutputDTOMapper;
+	}
+	
+	@RequestMapping("")
+	public ResponseEntity<Page<UserOutputDTO>> getAllUsers(
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "3") int size
+		) {
+
+		Page<UserOutputDTO> PageUserDTO = userService.getAllUsers(page, size).map(this.userOutputDTOMapper);
+
+		return new ResponseEntity<>(PageUserDTO, HttpStatus.OK);
 	}
 
 	@RequestMapping("/{id}")
--- src/main/java/es/ivborrezo/shoppinglistmonolith/user/UserService.java
+++ src/main/java/es/ivborrezo/shoppinglistmonolith/user/UserService.java
@@ -1,5 +1,8 @@
 package es.ivborrezo.shoppinglistmonolith.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import es.ivborrezo.shoppinglistmonolith.exception.ResourceNotFoundException;
@@ -13,6 +16,11 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
+	public Page<User> getAllUsers(int pageNumber, int pageSize){
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		return userRepository.findAll(pageable);
+	}
+	
 	public User getUserById(Long id) {
 		return userRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id, "getUserById", "id", id.toString()));
