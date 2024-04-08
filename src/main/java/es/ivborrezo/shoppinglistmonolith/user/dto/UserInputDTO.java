b04da802b2dc7454460c26cdec78a5fd85f682f1
= <=>
2024-04-08 22:04:51 +0200

    Add validation groups

--- src/main/java/es/ivborrezo/shoppinglistmonolith/user/dto/UserInputDTO.java
+++ src/main/java/es/ivborrezo/shoppinglistmonolith/user/dto/UserInputDTO.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import es.ivborrezo.shoppinglistmonolith.validationgroups.BasicValidation;
+import es.ivborrezo.shoppinglistmonolith.validationgroups.PatchValidation;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -17,27 +20,31 @@ import lombok.Setter;
 @Builder
 public class UserInputDTO {
 
-	@NotBlank(message = "name can not be empty")
+	@NotBlank(message = "name can not be empty", groups = BasicValidation.class)
 	private String name;
 	
-	@NotBlank(message = "email can not be empty")
+	@NotBlank(message = "email can not be empty", groups = BasicValidation.class)
+	@Email(groups = BasicValidation.class)
+	@Email(groups = PatchValidation.class)
 	private String email;
 
-	@NotBlank(message = "password can not be empty")
+	@NotBlank(message = "password can not be empty", groups = BasicValidation.class)
 	private String password;
 
-	@NotBlank(message = "firstName can not be empty")
+	@NotBlank(message = "firstName can not be empty", groups = BasicValidation.class)
 	private String firstName;
 
-	@NotBlank(message = "lastName can not be empty")
+	@NotBlank(message = "lastName can not be empty", groups = BasicValidation.class)
 	private String lastName;
 	
-	@NotNull(message = "dateOfBirth can not be empty")
-	@Past(message = "dateOfBirth must be in the empty")
+	@NotNull(message = "dateOfBirth can not be empty", groups = BasicValidation.class)
+	@Past(message = "dateOfBirth must be in the past", groups = BasicValidation.class)
+	@Past(message = "dateOfBirth must be in the past", groups = PatchValidation.class)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dateOfBirth;
 
-	@Pattern(regexp = "^$|^(\\+[0-9]{1,3})?[0-9]{8,14}$", message = "Invalid mobile phone format")
+	@Pattern(regexp = "^$|^(\\+[0-9]{1,3})?[0-9]{8,14}$", message = "Invalid mobile phone format", groups = BasicValidation.class)
+	@Pattern(regexp = "^$|^(\\+[0-9]{1,3})?[0-9]{8,14}$", message = "Invalid mobile phone format", groups = BasicValidation.class)
 	private String phoneNumber;
 	
 }
---
+++ src/main/java/es/ivborrezo/shoppinglistmonolith/validationgroups/PatchValidation.java
@@ -0,0 +1,5 @@
+package es.ivborrezo.shoppinglistmonolith.validationgroups;
+
+public interface PatchValidation {
+
+}
